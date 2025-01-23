46697dcb19e1013afb6608128e3ac013bf5478ac
Muhammad Usman <167646956+muhammadusman250@users.noreply.github.com>
2025-01-24 00:54:54 +0500

    Update client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -5,7 +5,7 @@ import { apiVersion } from '../env'
 export const client = createClient({
   projectId :"ftx2r4xa",
   dataset:"production",
-  token:"skMlcv2B4i4i7eSaJTQ4LWAaUULIJn9mIb1leBZlHswLYTYrVnKamid3fCdxnK1qHzZ0HR0lnwWOQnLbzFhQqzXyzVNlgHjT9OoWvhu7z83GlILvmd3YzDY3tfOrVKje8J6XdvCmFmvC4WPePXAkqhj7EMOYsp0gbYh2zGcr5RFQa6rfLA2t",
+  token:"sk836YvaBtFzNmmrsfUkA8aSrRroGN6NdRH7kP1I6em1XUY9zUWBfc8STwnwz7u3sKA8TEZqD8xkUsuJrsFXxny0n0z4Hbc0TIjE8KmlTxfVYiqzfmFPqM8KC2vc4Sdx90sTy6mtbOxi1pWcOqtfhXPmpXAehXCatjUmGK8eoo7Rj43wwyz9",
   apiVersion,
   useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
