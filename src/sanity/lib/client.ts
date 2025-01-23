a7f63e9932e72e7588a78b59c065f5c66c143f01
Muhammad Usman <167646956+muhammadusman250@users.noreply.github.com>
2025-01-24 00:41:56 +0500

    Update client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -7,5 +7,5 @@ export const client = createClient({
   dataset:"production",
   token:"skMlcv2B4i4i7eSaJTQ4LWAaUULIJn9mIb1leBZlHswLYTYrVnKamid3fCdxnK1qHzZ0HR0lnwWOQnLbzFhQqzXyzVNlgHjT9OoWvhu7z83GlILvmd3YzDY3tfOrVKje8J6XdvCmFmvC4WPePXAkqhj7EMOYsp0gbYh2zGcr5RFQa6rfLA2t",
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
